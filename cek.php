6f6ecc7c1aea1912bf6da734631be1292f01e895
imaen26 <imaen26@gmail.com>
2021-04-16 06:29:28 +0700

    ss
    sss

--- cek.php
+++ cek.php
@@ -1,12 +1,14 @@
 <?php
-        if (isset($_POST['cek'])) {
-            $nama_domain = "$_POST[domain]"."$_POST[ext]";
-            $arrHost = @gethostbynamel("$nama_domain");
-            if (empty($arrHost)) {
-                echo "<div class='hasildomain font-green'>Domain <u><b>$nama_domain</b></u> tersedia, Anda dapat menggunakan domain ini.</div>";
-            }
-            else {
-                echo "<div class='hasildomain font-red'>Domain <u><b>$nama_domain</b></u> sudah digunakan, coba dengan domain lain.</div>";
-            }
-        }
-        ?>
+if($_POST[proses]){
+    $nama_domain = "$_POST[domain]"."$_POST[suffix]";
+    $arrHost = @gethostbynamel("$nama_domain");
+       
+     if(empty($arrHost)){
+        echo "Domain $nama_domain Belum Ada Yang Punya";
+       
+    }else{
+        echo "Domain $nama_domain Sudah Ada Yang Punya";
+    }
+}
+
+?>
